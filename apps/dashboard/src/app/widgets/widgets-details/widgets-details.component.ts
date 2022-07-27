faa56c9da77674d5aaca5dc526ad03b735bc2834
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-07-27 17:58:09 +0200

    Copy from 01.1-boilerplate

--- apps/dashboard/src/app/widgets/widgets-details/widgets-details.component.ts
+++ apps/dashboard/src/app/widgets/widgets-details/widgets-details.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Widget } from '@fem/api-interfaces';
 
 @Component({
-  selector: 'fem-widgets-details',
+  selector: 'fem-widget-details',
   templateUrl: './widgets-details.component.html',
   styleUrls: ['./widgets-details.component.scss']
 })
-export class WidgetsDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class WidgetDetailsComponent {
+  currentWidget: Widget;
+  originalTitle = '';
+  @Input() set widget(value: Widget) {
+    if(value) this.originalTitle = value.title;
+    this.currentWidget = {...value};
+  };
+  @Output() saved = new EventEmitter;
+  @Output() cancelled = new EventEmitter;
 }
