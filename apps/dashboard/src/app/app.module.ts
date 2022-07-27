fc9fe1c598842a913e67230aa64ff328fe4bb536
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-07-27 16:55:43 +0200

    Create components, serv and modules needed

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -5,10 +5,27 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
+import { CoreDataModule, coreDataRoutes } from '@fem/core-data';
+import { CoreStateModule, coreStateRoutes } from '@fem/core-state';
+import { MaterialModule, materialRoutes } from '@fem/material';
+import { RoutingModule } from './routing.module';
+import { WidgetsComponent } from './widgets/widgets.component';
+import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
+import { WidgetsDetailsComponent } from './widgets/widgets-details/widgets-details.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, StoreModule.forRoot({}, {})],
+  declarations: [AppComponent, WidgetsComponent, WidgetsListComponent, WidgetsDetailsComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    CoreDataModule,
+    CoreStateModule,
+    MaterialModule,
+    RoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
