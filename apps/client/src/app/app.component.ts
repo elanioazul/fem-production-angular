c29ab7b7365a64f34dd6546e75433c74d39bc1c4
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-08-04 17:32:58 +0200

    Export one service from a lib an import in the new app

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { WidgetsService } from '@fem/core-data';
 @Component({
   selector: 'fem-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'client';
+  constructor(private widgetService: WidgetsService) {}
 }
