86b224b3077c6a3fda00002d13702b8d39474973
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-08-31 10:34:19 +0200

    All the way down

--- apps/dashboard/src/app/home/home.component.ts
+++ apps/dashboard/src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Widget } from '@fem/api-interfaces';
 import { WidgetsService } from '@fem/core-data';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'fem-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  widgets: Widget[] /*= [
+  widgets$: Observable<Widget[]> /*= [
     { id: '1', title: 'Widget 01', description: 'Pending' },
     { id: '2', title: 'Widget 02', description: 'Pending' },
     { id: '3', title: 'Widget 03', description: 'Pending' },
@@ -17,6 +18,6 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.widgetService.all().subscribe(widgets => this.widgets = widgets);
+    this.widgets$ = this.widgetService.all();
   }
 }
--- apps/dashboard/src/app/widgets/widgets.component.ts
+++ apps/dashboard/src/app/widgets/widgets.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Widget } from '@fem/api-interfaces';
 import { WidgetsService } from '@fem/core-data';
+import { Observable } from 'rxjs';
 
 const mockWidgets: Widget[] = [
   { id: '1', title: 'Widget 01', description: 'Pending' },
@@ -20,7 +21,8 @@ const emptyWidget: Widget = {
   styleUrls: ['./widgets.component.scss']
 })
 export class WidgetsComponent implements OnInit {
-  widgets: Widget[];
+  //widgets: Widget[];
+  widgets$: Observable<Widget[]>;
   selectedWidget: Widget;
 
   constructor(private widgetService: WidgetsService) {
@@ -45,36 +47,36 @@ export class WidgetsComponent implements OnInit {
   }
 
   loadWidgets() {
-    this.widgetService.all().subscribe(widgets => this.widgets = widgets);
+    //this.widgetService.all().subscribe(widgets => this.widgets = widgets);
+    this.widgets$ = this.widgetService.all();
   }
 
   saveWidget(widget: Widget) {
     if(widget.id) {
       this.updateWidget(widget);
     } else {
-      this.createWidget(widget);
+      this.createWidget(widget)
     }
   }
 
   createWidget(widget: Widget) {
-    const newWidget = Object.assign({}, widget, { id: this.getRandomID()})
-    this.widgets = [...this.widgets, newWidget];
+    //const newWidget = Object.assign({}, widget, { id: this.getRandomID()})
+    //this.widgets = [...this.widgets, newWidget];
+    this.widgetService.create(widget);
     this.resetForm();
   }
 
   updateWidget(widget: Widget) {
-    this.widgets = this.widgets.map(w => {
-      return (widget.id === w.id) ? widget : w;
-    });
+    // this.widgets = this.widgets.map(w => {
+    //   return (widget.id === w.id) ? widget : w;
+    // });
+    this.widgetService.update(widget);
     this.resetForm();
   }
 
   deleteWidget(widget: Widget) {
-    this.widgets = this.widgets.filter(w => widget.id !== w.id);
+    //this.widgets = this.widgets.filter(w => widget.id !== w.id);
+    this.widgetService.delete(widget);
     this.resetForm();
-  }
-
-  private getRandomID() {
-    return Math.random().toString(36).substring(7);
   }
 }
